4a1261c54b9ee5b9538cba125e34bc44f96f4fd6
Daniel Buitrago <daniel.buitrago@wnco.com>
2020-03-26 21:11:02 -0500

    Initial commit

---
+++ restful-webservices/src/test/java/com/webservices/restfulwebservices/RestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
