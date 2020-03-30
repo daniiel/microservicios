b1c3662c79c647e56dab494d81ae0cfe34279896
Daniel Buitrago <daniel.buitrago@wnco.com>
2020-03-29 20:42:21 -0500

    Implemented spring-actuator

--- restful-webservices/src/main/java/com/webservices/restfulwebservices/config/SwaggerConfig.java
+++ restful-webservices/src/main/java/com/webservices/restfulwebservices/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.webservices.restfulwebservices;
+package com.webservices.restfulwebservices.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
